6480cac4120a8c4f5bf8d6f1f04aaba4eb540c8b
Anupam <anusrivastava910@gmail.com>
2022-02-21 15:16:29 +0530

    Add movie-booking intial commit

---
+++ movie-booking/src/test/java/com/anuanu00/moviebooking/AppTest.java
@@ -0,0 +1,13 @@
+package com.anuanu00.moviebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
