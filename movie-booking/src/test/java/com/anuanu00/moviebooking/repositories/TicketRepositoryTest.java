d090c36b8ccd736a440754abc0de09eaa8cf8535
Anupam <anusrivastava910@gmail.com>
2022-02-24 15:46:57 +0530

    Add movie-booking repository tests

--- movie-booking/src/test/java/com/anuanu00/moviebooking/repositories/TicketRepositoryTest.java
+++ movie-booking/src/test/java/com/anuanu00/moviebooking/repositories/TicketRepositoryTest.java
@@ -1,9 +1,102 @@
 package com.anuanu00.moviebooking.repositories;
 
+import com.anuanu00.moviebooking.entites.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @DisplayName("TicketRepositoryTest")
 public class TicketRepositoryTest {
     private TicketRepository ticketRepository;
 
+    @BeforeEach
+    public void setup() throws ParseException {
+        Map<Integer, Ticket> ticketMap = new HashMap<>();
+        Customer customer = new Customer("1", "CustomerA", "test1@gmail.com");
+        Show show = new Show("1",  new SimpleDateFormat("dd/MM/yyyy HH:mm").parse("20/07/2021 13:30"),
+                new SimpleDateFormat("dd/MM/yyyy HH:mm").parse("20/07/2021 16:00"),
+                new Screen("1","ScreenA"),
+                new Cinema("1","CinemaA"),
+                new Movie("1","MovieA",120));
+        Seat seat1_2 = new Seat("1#2", 1, 2);
+        Seat seat1_3 = new Seat("1#3", 1, 3);
+        List<Seat> seatList = new ArrayList<>(List.of(seat1_2, seat1_3));
+
+       Ticket ticket = new Ticket(1, customer, show, seatList);
+       ticketMap.put(1, ticket);
+
+       ticketRepository = new TicketRepository(ticketMap, 1);
+    }
+
+    @Test
+    @DisplayName("getTicketId method Given Ticket Id Should Return Ticket")
+    public void getTicketId_GivenTicketId_ShouldReturnTicket() throws ParseException {
+        // Arrange
+        Customer customer = new Customer("1", "CustomerA", "test1@gmail.com");
+        Show show = new Show("1",  new SimpleDateFormat("dd/MM/yyyy HH:mm").parse("20/07/2021 13:30"),
+                new SimpleDateFormat("dd/MM/yyyy HH:mm").parse("20/07/2021 16:00"),
+                new Screen("1","ScreenA"),
+                new Cinema("1","CinemaA"),
+                new Movie("1","MovieA",120));
+        Seat seat1_2 = new Seat("1#2", 1, 2);
+        Seat seat1_3 = new Seat("1#3", 1, 3);
+        List<Seat> seatList = new ArrayList<>(List.of(seat1_2, seat1_3));
+
+        Ticket expectedTicket = new Ticket(1, customer, show, seatList);
+        // Act
+        Ticket actualTicket = ticketRepository.getTicketById(1);
+        // Assert
+        Assertions.assertEquals(expectedTicket, actualTicket);
+    }
+
+    @Test
+    @DisplayName("getTicketId method Given Ticket Id Should Return Null")
+    public void getTicketId_GivenTicketId_ShouldReturnNullIfNotPresent() {
+        // Arrange
+        Integer ticketId = 2;
+        // Act
+        Ticket expectedTicket = ticketRepository.getTicketById(ticketId);
+        // Assert
+        Assertions.assertNull(expectedTicket);
+    }
+
+    @Test
+    @DisplayName("saveTicket Method Should Save Ticket")
+    public void saveTicket_ShouldSaveTicket() throws ParseException {
+        // Arrange
+        Customer customer = new Customer("2", "CustomerB", "test2@gmail.com");
+        Show show = new Show("1",  new SimpleDateFormat("dd/MM/yyyy HH:mm").parse("20/07/2021 13:30"),
+                new SimpleDateFormat("dd/MM/yyyy HH:mm").parse("20/07/2021 16:00"),
+                new Screen("1","ScreenA"),
+                new Cinema("1","CinemaA"),
+                new Movie("1","MovieA",120));
+        Seat seat2_1 = new Seat("2#1", 2, 1);
+        Seat seat2_2 = new Seat("2#2", 2, 2);
+        List<Seat> seatList = new ArrayList<>(List.of(seat2_1, seat2_2));
+
+        Ticket expectedTicket = new Ticket(2, customer, show, seatList);
+        // Act
+        ticketRepository.saveTicket(customer, show, seatList);
+        // Assert
+        Assertions.assertEquals(expectedTicket, ticketRepository.getTicketById(2));
+    }
+
+    @Test
+    @DisplayName("removeTicket Method Should Remove Ticket")
+    public void removeTicket_ShouldRemoveTicket() {
+        // Arrange
+        Integer ticketId = 1;
+        // Act
+        ticketRepository.removeTicket(ticketId);
+        // Assert
+        Assertions.assertNull(ticketRepository.getTicketById(1));
+    }
 }
