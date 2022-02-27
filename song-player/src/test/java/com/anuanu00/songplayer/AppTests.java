26762cd6a72e71230558edb0af577e942598a340
Anupam <anusrivastava910@gmail.com>
2022-02-27 22:03:31 +0530

    Add song-player project structure

--- song-player/src/test/java/com/anuanu00/songplayer/AppTests.java
+++ song-player/src/test/java/com/anuanu00/songplayer/AppTests.java
@@ -1,13 +1,12 @@
 package com.anuanu00.songplayer;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class AppTests {
+//@SpringBootTest
 
-    @Test
-    void contextLoads() {
-    }
+@DisplayName("Integration tests")
+class AppTests {
 
 }
